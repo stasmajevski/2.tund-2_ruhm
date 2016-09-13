e8f6c8fb2d70194ec2f06d2974f01d3e4ce02253
stanmaje <stanmaje@greeny.cs.tlu.ee>
2016-09-13 13:18:11 +0300

    added for loop

---
+++ second_lesson.php
@@ -0,0 +1,23 @@
+<?php
+
+$age = 5;
+
+if($age < 18){
+	
+	echo "NOT ALLOWED";
+	
+}
+else{
+	
+	echo "OK";
+}
+echo "<br>";	
+// eesmark vanusele vastavalt trukime valja 'palju'
+
+for($i = 1;$i<=$age;$i++){
+	echo "palju<br> $i ";
+}
+
+echo "onne";
+
+?>
